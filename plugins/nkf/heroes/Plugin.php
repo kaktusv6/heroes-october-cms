e847a5b33bf4292442af7e0c129233c8512f91ef
kaktusv6 <kaktusv6@gmail.com>
2020-02-03 20:35:06 +1000

    Change methods of plugin

--- plugins/nkf/heroes/Plugin.php
+++ plugins/nkf/heroes/Plugin.php
@@ -1,14 +1,26 @@
-<?php namespace Nkf\Heroes;
+<?php declare(strict_types=1);
 
+namespace Nkf\Heroes;
+
+use Nkf\Heroes\Console\Seed;
 use System\Classes\PluginBase;
 
 class Plugin extends PluginBase
 {
     public function registerComponents()
     {
+
     }
 
     public function registerSettings()
     {
     }
+
+    public function register()
+    {
+        parent::register();
+        $this->registerConsoleCommand('heroes:name', Seed::class);
+    }
+
+
 }
