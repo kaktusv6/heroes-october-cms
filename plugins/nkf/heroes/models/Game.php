a466e5691fe15d920ff4296955d947c9d6bba5d0
Vasiliy <nkf@feip.co>
2020-02-17 09:24:16 +1000

    Create method get tables

--- plugins/nkf/heroes/models/Game.php
+++ plugins/nkf/heroes/models/Game.php
@@ -2,6 +2,7 @@
 
 namespace Nkf\Heroes\Models;
 
+use Illuminate\Support\Facades\DB;
 use Model;
 use October\Rain\Database\Traits\Validation;
 
@@ -25,4 +26,16 @@ class Game extends Model
     public $rules = [
         'title' => 'required|max:255',
     ];
+    public $jsonable = ['entities'];
+
+    public function getEntityOptions(): array
+    {
+        return DB::table('information_schema.COLUMNS')
+            ->select('TABLE_NAME')
+            ->where('TABLE_SCHEMA', 'LIKE', 'heroes', 'and')
+            ->where('COLUMN_NAME', 'LIKE', 'game_id')->get()
+            ->map(function ($val) {
+                return $val->TABLE_NAME;
+            })->toArray();
+    }
 }
