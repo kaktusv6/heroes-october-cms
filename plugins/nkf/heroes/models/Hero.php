1368c54067a90112d16e7484c05b28861376cd5b
Vasiliy <nkf@feip.co>
2020-02-17 09:53:13 +1000

    Create heroes

---
+++ plugins/nkf/heroes/models/Hero.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+namespace Nkf\Heroes\Models;
+
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Model;
+use October\Rain\Database\Traits\Validation;
+
+/**
+ * hero Model
+ *
+ * @property int $id
+ * @property string $name
+ * @property int $game_id
+ * @property-read \Nkf\Heroes\Models\Game $game
+ * @method static \Illuminate\Database\Eloquent\Builder|\Nkf\Heroes\Models\Hero whereGameId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Nkf\Heroes\Models\Hero whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Nkf\Heroes\Models\Hero whereName($value)
+ * @mixin \Eloquent
+ */
+class Hero extends Model
+{
+    use Validation;
+
+    public $timestamps = false;
+    public $table = 'nkf_heroes_heroes';
+    public $rules = [
+        'name' => 'required',
+        'game_id' => 'required|min:1',
+    ];
+
+    public $belongsTo = ['game' => Game::class];
+    public function game(): BelongsTo
+    {
+        return $this->belongsTo(Game::class);
+    }
+}
