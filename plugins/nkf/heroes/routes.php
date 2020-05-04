b24c396b9e468856878b4b0aea2e738ca74d79e1
Vasiliy <nkf@feip.co>
2020-05-04 18:01:08 +1000

    Change type requests

--- plugins/nkf/heroes/routes.php
+++ plugins/nkf/heroes/routes.php
@@ -9,13 +9,13 @@ use Nkf\Heroes\Api\Controllers\UserController;
 
 Route::group(['prefix' => 'api', 'middleware' => ApiKey::ALIAS], function () {
     Route::get('/', TestController::class . '@test');
-    Route::get('/user/register', UserController::class . '@register');
-    Route::get('/user/login', UserController::class . '@login');
+    Route::post('/user/register', UserController::class . '@register');
+    Route::post('/user/login', UserController::class . '@login');
 
     Route::group(['middleware' => TokenAuthenticate::ALIAS], function () {
         Route::get('games', GameController::class . '@games');
         Route::get('test', TestController::class . '@test');
-        Route::get('/user/logout', UserController::class . '@logout');
+        Route::post('/user/logout', UserController::class . '@logout');
 
         Route::get('heroes', HeroController::class . '@heroesUser');
         Route::group(['prefix' => 'hero'], function () {
